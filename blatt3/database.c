cf249d4e8703c5ae73408704aedf5b360cc60c3c
change_your_name <change_your_email_adress@medieninf.de>
2018-05-06 22:36:24 +0200

    db_put, db_get and db_list

--- blatt3/database.c
+++ blatt3/database.c
@@ -7,11 +7,25 @@
 #include "database.h"
 
 int db_put(const char *filepath, int index,const DBRecord *record){
+	DBRecord newEntry;
+	int writefd;
+
+	strcpy(newEntry.key,record->key);
+	strcpy(newEntry.cat,record->cat);
+	strcpy(newEntry.value,record->value);
+
+	if(index == -1){
+		writefd = open(filepath,O_WRONLY|O_APPEND);
+		write(writefd,record,sizeof(DBRecord));
+		close(writefd);
+	}
+
 	return 0;
 }
 
-int db_list(const char *path, int outfd, int (*filter)(DBRecord *rec, const void *data),const void *data){
-	int readfd = open(path,O_WRONLY);
+int db_get(const char * filepath,int index, DBRecord *result){
+	int readfd = open(filepath,O_RDONLY);
+	int r,s;
 	DBRecord readin;
 
 	if(readfd < 0){
@@ -19,23 +33,77 @@ int db_list(const char *path, int outfd, int (*filter)(DBRecord *rec, const void
 		exit(2);
 	}
 
-	if(filter == NULL){
-		while(1){
-			read(readfd,&readin,sizeof(DBRecord));
-			write(1,&(readin.key),strlen(readin.key));
-			write(1,&(readin.cat),strlen(readin.cat));
-			write(1,&(readin.value),strlen(readin.value));
-			write(1,"\n",1);
-			close(readfd);
-		}
-	}
+	s = lseek(readfd,sizeof(DBRecord),SEEK_SET+index-1);
+	r = read(readfd,&(readin),sizeof(DBRecord));
+
+	strcpy(result->key,readin.key);
+	strcpy(result->cat,readin.cat);
+	strcpy(result->value,readin.value);
+
+	close(readfd);
+	return 0;
+	
+}
+
+int formatedPrint(DBRecord record,int outfd){
+	char spaces[]= "                   ";
+
+	write(outfd,record.key,strlen(record.key));
+	write(outfd,spaces,strlen(spaces)-strlen(record.key));
+
+	write(outfd,"|",1);
+
+	write(outfd,record.cat,strlen(record.cat));
+	write(outfd,spaces,strlen(spaces)-strlen(record.cat));
+
+	write(outfd,"|",1);
+	
+	write(outfd,record.value,strlen(record.value));
+	write(outfd,"\n",1);
 
 	return 0;
 }
 
+int db_list(const char *path, int outfd, int (*filter)(DBRecord *rec, const void *data),const void *data){
+	int readfd = open(path,O_RDONLY);
+	int r;
+	DBRecord readin;
+
+	if(readfd < 0){
+		perror("Probleme beim öffnen der datei\n");
+		exit(2);
+	}
+
+	while(1){
+		r = read(readfd,&(readin), sizeof(DBRecord));
+
+		if(r == 0){
+			break;
+		}else if(r <0){
+			perror("Lesefehler");
+			close(readfd);
+			return -1;
+		}
+
+		formatedPrint(readin,outfd);
+
+	}
+		close(readfd);
+		return 0;
+	}
+
+	
+
+
 
 int main(int argc, char const *argv[])
 {
+	DBRecord res = {"FOURTH","ENTRY","Mein vierter Eintrag."};
+	DBRecord *pres = &res;
 	db_list("first.db",1,NULL,NULL);
+	
+	db_get("first.db",0,pres);
+	formatedPrint(*pres,1);
+
 	return 0;
 }
