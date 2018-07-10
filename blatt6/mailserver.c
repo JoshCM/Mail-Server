578dceb07ea4adf91781a62d7584d19a7ae05020
joshua.mestre@outlook.com <joshua.mestre@outlook.com>
2018-07-10 12:37:39 +0200

    lets fix this in da uni

--- blatt6/mailserver.c
+++ blatt6/mailserver.c
@@ -129,9 +129,12 @@ int main(void)
                 perror("accept");
                 exit(-1);
             }
+            process_smtp(newsockfd,newsockfd);
+            /*
             pthread_create(&tid[zaehler], NULL, createThread, &newsockfd);
             pthread_detach(zaehler);
             zaehler++;
+            */
         }
     }
     return 0;
